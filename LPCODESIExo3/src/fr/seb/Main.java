b50c807444da7003618cd49c5fb7fd28032682bb
Xavire La <xavierla74@hotmail.fr>
2019-02-15 16:50:03 +0100

    retry

--- LPCODESIExo3/src/fr/seb/Main.java
+++ LPCODESIExo3/src/fr/seb/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  Fichier monFichier = new Fichier();
+  Fichier monFichie = new Fichier();
   Repertoire monRepertoire = new Repertoire();
   SystemeFichier monSystemeFichier = new SystemeFichier();
+
 
 	}
 
