65114ab120bb8898526800df0fd4a73a16e3f405
Xavire La <xavierla74@hotmail.fr>
2019-02-15 16:42:47 +0100

    Création du main

---
+++ LPCODESIExo3/src/fr/seb/Main.java
@@ -0,0 +1,13 @@
+package fr.seb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Fichier monFichier = new Fichier();
+  Repertoire monRepertoire = new Repertoire();
+  SystemeFichier monSystemeFichier = new SystemeFichier();
+
+	}
+
+}
